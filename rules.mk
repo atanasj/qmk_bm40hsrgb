98bb284444bf9b0952e2105d9b3b5cab2bffbfd4
atanas <atanas.atanas@gmail.com>
2021-05-10 00:29:47 +1000

    update combo script dir

--- rules.mk
+++ rules.mk
@@ -2,7 +2,7 @@ LTO_ENABLE = yes				# reduces files size dramatically
 # uses lite by default, if need this feature come back here
 # BOOTMAGIC_ENABLE = full
 # keep path to combos local to my keymap
-VPATH  +=  keyboards/bm40hsrgb/keymaps/atanas_def/g/
+VPATH  +=  keyboards/bm40hsrgb/keymaps/atanas/g/
 COMBO_ENABLE = yes
 MOUSEKEY_ENABLE = yes
 TAP_DANCE_ENABLE = yes
